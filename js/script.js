68ad585889b0c81b4d379a895be8dd07882bff43
Felipe S Bezerra <felipecrf220@gmail.com>
2023-07-09 21:08:00 -0300

    adicionado Media Queries para telas com width menores que 480px

--- js/script.js
+++ js/script.js
@@ -1,14 +1,30 @@
 
+let icon_btn = document.querySelector("#icon-btn");
+
 // entra na página inicial sempre que o html é iniciado
+// Se for em um dispositivo com width < 480, altera o ícone de expandir a sidebar
 window.onload = function() {
     document.querySelector("#link-inicio").click();
+    var w = window.innerWidth || document.documentElement.clientHeight || document.body.clientWidth;
+    if (w < 480) {
+        document.body.classList.add("collapsed");
+        icon_btn.textContent = "menu";
+    }
 }
 
-// Colapsa a sidebar sempre que a tela do navegador for reduzida a valores manores que 800px
+// Colapsa a sidebar sempre que a tela do navegador for reduzida a valores menores que 800px
+// alterna o ícone do botão de expandir a sidebar
 window.onresize = function() {
     var w = window.innerWidth || document.documentElement.clientHeight || document.body.clientWidth;
     if (w < 800) {
         document.body.classList.add("collapsed");
+        if (w <= 480) {
+            icon_btn.textContent = "menu";
+        } else {
+            if ( icon_btn.textContent == "menu") {
+             icon_btn.textContent = "chevron_left";
+            }
+        } 
     }
 }
 
@@ -19,6 +35,16 @@ const expand_btn = document.querySelector(".expand-btn");
 //e adiciona ou remove a classe collapsed do body sempre que o botão é clicado
 expand_btn.addEventListener('click', () => {
   document.body.classList.toggle("collapsed");
+
+  // alterna o ícone do botão de expandir a sidebar
+  var w = window.innerWidth || document.documentElement.clientHeight || document.body.clientWidth;
+  if (w <= 480) {
+    if ( icon_btn.textContent == "menu") {
+        icon_btn.textContent = "chevron_left";
+    } else {
+        icon_btn.textContent = "menu"
+    }
+  }
 });
 
 // Obtém o href atual
